7d5bac6046a7063491c0438d39390d83e7460f28
danielle revels <ddaniellerrevels@aol.com>
2020-08-12 03:47:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,2 @@
-var evens = [0,2,4,6,8,10];
 
-evens.forEach(even => {
-  console.log(`${even} is not odd!`);
-});
-
-var evens = [0,2,4,6,8,10];
-
-evens.forEach((even,index,array)=> {
-  debugger;
-  console.log(`${even} is not odd`)
-})
-
-var evens = [0, 2, 4, 6, 8, 10];
- 
-function evenFn(even, index, array) {
-  console.log(`${even} is not odd!`);
-}
- 
-for (let i = 0; i < evens.length; i++) {
-  evenFn(evens[i], i, evens);
-}
-
-var evens = [0, 2, 4, 6, 8, 10];
- 
-function doToEvens(callback) {
-  evens.forEach(callback);
-}
 // Add your changeCompletely() function here:
